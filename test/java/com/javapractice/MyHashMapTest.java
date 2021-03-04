1e2036b4449aa82322780a60f9e59791595d2f18
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-04 19:52:02 +0530

    Tested : Wrote Junit test for to check frequency of words in sentences

--- test/java/com/javapractice/MyHashMapTest.java
+++ test/java/com/javapractice/MyHashMapTest.java
@@ -4,7 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MyHashMapTest {
-        public void welcome(){
-            System.out.println("Welcome message");
+    @Test
+    public void givenASentencesWhenWordsAreAddedToListShouldReturnWordFrequency() {
+        String sentence = "To be or not to be";
+        MyHashMap<String,Integer> myHashMap = new MyHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word : words) {
+            Integer value = myHashMap.get(word);
+            if (value == null) value = 1;
+            else value = value + 1;
+            myHashMap.add(word, value);
         }
+        int frequency = myHashMap.get("to");
+        System.out.println(myHashMap);
+        Assertions.assertEquals(2,frequency);
+    }
 }
