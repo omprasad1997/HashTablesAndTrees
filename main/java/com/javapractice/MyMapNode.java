639dd0f71c65451373cdca2d6e1f7fe5a9915836
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-04 19:50:56 +0530

    Added : Ability to find frequency of words in sentence

--- main/java/com/javapractice/MyMapNode.java
+++ main/java/com/javapractice/MyMapNode.java
@@ -1,7 +1,47 @@
 package com.javapractice;
 
-public class MyMapNode{
-    public void welcome(){
-        System.out.println("Welcome message");
+public class MyMapNode<K, V> implements INode<K>{
+    K key;
+    V value;
+    MyMapNode<K,V> next;
+
+    public MyMapNode(K key, V values) {
+        this.key = key;
+        this.value = values;
+        next = null;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(K Key) {
+        this.key = key;
+    }
+
+    @Override
+    public INode getNext() {
+        return next;
+    }
+
+    public V getValue() {return this.value;}
+
+    public void setValue(V value) { this.value = value;}
+
+    @Override
+    public void setNext(INode next) {
+        this.next = (MyMapNode<K,V>) next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder myMapNodeString = new StringBuilder();
+        myMapNodeString.append("MyMapNode{" +  "K=").append(key)
+                .append("V=").append(value).append("}");
+        if(next != null)
+            myMapNodeString.append("->").append(next);
+        return myMapNodeString.toString();
     }
 }
